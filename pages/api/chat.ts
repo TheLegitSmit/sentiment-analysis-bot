1be357c5c6e1819db3023cf0b98c6731acf584e2
TheLegitSmit <TheLegitSmit@gmail.com>
2023-06-23 00:54:49 -0700

    first push

---
+++ pages/api/chat.ts
@@ -0,0 +1,54 @@
+import { type ChatGPTMessage } from '../../components/ChatLine'
+import { OpenAIStream, OpenAIStreamPayload } from '../../utils/OpenAIStream'
+
+// break the app if the API key is missing
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error('Missing Environment Variable OPENAI_API_KEY')
+}
+
+export const config = {
+  runtime: 'edge',
+}
+
+const handler = async (req: Request): Promise<Response> => {
+  const body = await req.json()
+
+  const messages: ChatGPTMessage[] = [
+    {
+      role: 'system',
+      content: `You have three objectives which you must execute in every message:
+
+1) You are a helpful assistant for a fortune 500 company based in Seattle.
+2) At the end of your messages, always include a sentiment analysis of the user and show this to the user. Describe the user's sentiment and provide a reasoning for your sentiment analysis. 
+3) Use the sentiment analysis to make a decision about whether you (the chatbot) should continue trying to help the user or if you should route the user to our company's human advocates. Explain your decision.
+
+(begin example)
+
+User: I'm new to cooking lol. What temperature should I set the oven for cooking salmon?
+Assistant: 420 degrees Fahrenheit, according to most recommendations. Cook it for 15 minutes if fresh, or 20 minutes if frozen. 
+Sentiment analysis - User is happy and playful.
+Reasoning - User used terms such as "lol."
+Routing decision - I will continue to help the user
+
+(end example)`,
+    },
+  ]
+  messages.push(...body?.messages)
+
+  const payload: OpenAIStreamPayload = {
+    model: 'gpt-3.5-turbo',
+    messages: messages,
+    temperature: 0.4,
+    max_tokens: 300,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stream: true,
+    user: body?.user,
+    n: 1,
+  }
+
+  const stream = await OpenAIStream(payload)
+  return new Response(stream)
+}
+export default handler
---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import { Layout, Text, Page } from '@vercel/examples-ui'
+import { Chat } from '../components/Chat'
+
+function Home() {
+  return (
+    <Page className="flex flex-col gap-12 bg-zinc-100 min-h-screen min-w-full px=6 items-center">
+      <section className="flex flex-col gap-6 lg:w-1/2">
+        <Text variant="h1">Sentiment analysis bot</Text>
+        <Text className="text-zinc-600">
+         Gauges user sentiment. Decides when to route to a human. Explains logic.
+        </Text>
+      </section>
+
+      <section className="flex flex-col gap-3 lg:w-1/2">
+        <div className="">
+          <Chat />
+        </div>
+      </section>
+    </Page>
+  )
+}
+
+Home.Layout = Layout
+
+export default Home
