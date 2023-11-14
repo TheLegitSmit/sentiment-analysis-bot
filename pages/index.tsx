2f7aecae9eb337703fe689883a5c43d6ded3c469
root <root@Pasha>
2023-11-14 13:13:53 +0200

    Fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { Chat } from '../components/Chat'
 
 function Home() {
   return (
-    <Page className="flex flex-col gap-12 bg-zinc-100 min-h-screen min-w-full px=6 items-center">
+    <Page className="flex flex-col px-5 gap-12 bg-zinc-100 min-h-screen min-w-full px=6 items-center">
       <section className="flex flex-col gap-6 lg:w-1/2">
         <Text variant="h1">Sentiment analysis bot</Text>
         <Text className="text-zinc-600">
